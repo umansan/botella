7eb9e4fff8fe1eadce7875ecced47f375a027f6f
jobiols <jorge.obiols@gmail.com>
2019-10-24 19:34:30 -0300

    [FIX] repositorios uno por uno

--- rsync_files.sh
+++ rsync_files.sh
@@ -2,20 +2,30 @@
 # copia los repositorios de la instalacion local que se construyen con oe
 # elimina el .git porque sino los toma como submodules
 
+ORIGIN="/odoo_ar/odoo-12.0e/botella/sources"
+
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/account-financial-tools ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/account-invoicing ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/account-payment ./ --exclude=.git --exclude=setup/
 
-rsync -r /odoo_ar/odoo-12.0e/botella/sources/aeroo_reports/report_aeroo ./aeroo_reports --exclude=.git --exclude=setup/
-rsync -r /odoo_ar/odoo-12.0e/botella/sources/aeroo_reports/requirements.txt ./aeroo_reports
+REPO="aeroo_reports"
+rsync -r $ORIGIN/$REPO/requirements.txt ./$REPO/
+rsync -r $ORIGIN/$REPO/report_aeroo ./$REPO/ --exclude=setup/
 
-#rsync -r /odoo_ar/odoo-12.0e/botella/sources/argentina-reporting ./ --exclude=.git --exclude=setup/
+# ARGENTINA REPORTING
+#rsync -r /odoo_ar/odoo-12.0e/botella/sources/argentina-reporting/requirements.txt ./argentina-reporting
+#rsync -r /odoo_ar/odoo-12.0e/botella/sources/argentina-reporting ./
+
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/argentina-sale ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/management-system ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/manufacture ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/miscellaneous ./ --exclude=.git --exclude=setup/
-#rsync -r /odoo_ar/odoo-12.0e/botella/sources/odoo-argentina ./ --exclude=.git --exclude=setup/
-#rsync -r /odoo_ar/odoo-12.0e/botella/sources/odoo-support ./ --exclude=.git --exclude=setup/
+
+REPO="odoo-argentina"
+rsync -r $ORIGIN/$REPO/requirements.txt ./$REPO/
+rsync -r $ORIGIN/$REPO/l10n_ar_base ./$REPO/
+
+# rsync -r /odoo_ar/odoo-12.0e/botella/sources/odoo-support ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/partner-contact ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/product ./ --exclude=.git --exclude=setup/
 #rsync -r /odoo_ar/odoo-12.0e/botella/sources/reporting-engine ./ --exclude=.git --exclude=setup/
