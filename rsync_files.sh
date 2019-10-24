7ac28604ad800b0f7f25bfee96406b0dd77b4362
jobiols <jorge.obiols@gmail.com>
2019-10-24 14:39:45 -0300

    [ADD] repo

--- rsync_files.sh
+++ rsync_files.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 rsync -r /odoo_ar/odoo-12.0e/botella/sources/odoo-argentina/ ./odoo-argentina --exclude=.git
+rsync -r /odoo_ar/odoo-12.0e/botella/sources/account-payment ./ --exclude=.git
